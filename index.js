2db2b9defc3224a1c20dcb0df644ca8a09be5ac3
Ryan Brady <ryanbrady369@gmail.com>
2018-11-01 23:04:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function distanceTravelledInFeet(start, end) {
 function calculatesFarePrice(start, end) {
   let price = 0
 
-  if (distanceFromHqInFeet(start, end) < 400) return price
-  if (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) < 2000) return price = (distanceTravelledInFeet(start, end) - 400) * .02
+  if (distanceFromHqInFeet(start, end) < 400) return 0
+  if (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) < 2000) return (distanceTravelledInFeet(start, end) - 400) * .02
   if (distanceTravelledInFeet(start, end) > 2500) return 'cannot travel that far'
+  return 25
 }
